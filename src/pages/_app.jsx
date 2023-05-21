d41edd662125db66fc57a2c621ce67ef12f9ca5d
Nadhif Fuadi <nadhiffuadi@gmail.com>
2023-05-21 18:31:58 +0700

    fix module not found

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,12 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.css'
-import '@/styles/globals.css'
+import '../styles/Globals.css'
 import '../styles/Home.css'
 import '../styles/Register.css'
-import '../styles/media.css'
-import '../styles/landing.css'
-// import "@/styles/Login.css";
-// import "../styles/GameList.css";
-// import "../styles/ItemCard.css";
+import '../styles/Media.css'
+import '../styles/Landing.css'
 import { Provider } from 'react-redux'
 import store from '../store/store'
 
